b6d6ba5686b70404fb282d38896f0575d50b480f
Edgar Gomes <edgargomes.iff@gmail.com>
2022-04-19 14:51:01 -0300

    Adicionando mensagem de carregamento enquando é feita a leitura de dados na API.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, {useState, useEffect} from "react"
 import InputSearch from "./InputSearch"
 import './App.css';
- const api = 'https://kitsu.io/api/edge/'
+const api = 'https://kitsu.io/api/edge/'
 
 function App() {
   //inicializando state que será utilizada para monitorar o texto inserido pelo usuário
   const [text, setText] = useState('')
-   //inicializando state que será utilizada para monitorar as informações dos dados da API.
+   //inicializando state que será utilizada para receber os dados da API.
   const [info, setInfo] = useState({})
  
  //Pesquisando dados correspondentes ao input na API a cada mudança no state de text
   useEffect(()=>{
     if(text){
+      //resetando info para que haja exibição de mensagem de carregamento mesmo que o input não esteja vazio
+      setInfo({})
       fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
         .then((response)=>response.json())
           .then((response)=>{setInfo(response)})
-
     }
   }, [text])
+
+
   return (
     <div className="App">
+
       <h1>Animes</h1>
       {/*Chamando componente que irá setar o text com o valor imposto pelo usuário*/}
       <InputSearch value={text} onChange={(search)=> setText(search)}/>
-       {/*listando dados correspondentes a pesquisa (caso exista algum valor sendo retornado pela API)*/}
+
+      {/*Imporimindo mensagem de carregamento enquanto estiver sendo feita a leitura de dados na API*/}
+      {text && !info.data && (
+        <span>Carregando...</span>
+       )}
+
+      {/*listando dados correspondentes a pesquisa (caso exista algum valor sendo retornado pela API)*/}
       {info.data && (
         <ul className="animes-list">
           {info.data.map((anime)=>(
