a5a48b22420878e8aa00e79dd5549dc310acaf5e
Edgar Gomes <edgargomes.iff@gmail.com>
2022-04-24 14:21:51 -0300

    Implementando Route para definir a página de início como '/'.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+    <Route exact path="/" element={<App/>}/>
+    <Route path="*" element={(
+        <p style={{padding: "3rem"}}>
+          A página que você tentou acessar não existe!
+        </p>
+      )}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
