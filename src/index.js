abe0cd2522105625246caaf4179a9635b024ba3b
Edgar Gomes <edgargomes.iff@gmail.com>
2022-04-25 10:05:04 -0300

    Desimplementando uso de rotas.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-    <Route exact path="/Home" element={<App/>}/>
-    <Route path="*" element={(
-        <p style={{padding: "3rem"}}>
-          A página que você tentou acessar não existe! <br/>
-          Talvez você esteja tentando ir para <Link to="/Home">Home</Link>
-        </p>
-      )}/>
-    </Routes>
-  </BrowserRouter>
+  <App/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
